a30248fd1e5d68a801a0a20b7915a0b161f53f83
joa-nna <74781386+joa-nna@users.noreply.github.com>
2021-06-01 10:49:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ Created on Wed Jan 29 10:29:55 2020
 
 #insert local parameters
 
-username = 'your-spotify-username'
-client_id ='your-client-id'
-client_secret = 'your-client-secret'
+username = 'bananasplitoh'
+client_id ='dc6196f749ae45d0a582814525ab8758'
+client_secret = ' fb2dc47f056a403ab6790962113555ca'
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
+
